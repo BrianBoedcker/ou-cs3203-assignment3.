217ce3f8e72c506fab6834a0e139bd3466fd2874
BrianBoedecker <brian.t.boedecker-1@ou.edu>
2023-09-13 20:56:52 -0500

    Added addArray Functionality

--- Assignment 3/main.cpp
+++ Assignment 3/main.cpp
@@ -6,9 +6,31 @@
 //
 
 #include <iostream>
+using namespace std;
+ 
+int addArray(int arr[], int size);
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main ()
+{
+    int array[5] = {1000,5, 3, 17, 50};
+    int sum;
+
+    sum = addArray( array, 5 );
+ 
+    cout << "The sum of the numbers in the array is:  " << sum << endl;
+
     return 0;
+}
+
+int addArray(int arr[], int size)
+{
+    int sum;
+    sum = 0;
+
+    for (int x = 0; x < size; x++)
+    {
+        sum += arr[x];
+    }
+
+    return sum;
 }
